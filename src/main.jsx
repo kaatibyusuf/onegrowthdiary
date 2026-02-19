865390934f39c1efc1e02f6d12f09eece3184b9f
Abdulsamad Yusuf <abdulsamadolohuntosinyusuf@gmail.com>
2026-02-19 08:55:39 +0100

    chore: configure GitHub Pages deployment for Vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-import { AuthProvider } from "./lib/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthProvider>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
